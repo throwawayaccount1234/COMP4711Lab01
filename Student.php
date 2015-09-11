bf3abe090167dd9ce7ef628c3f8e313001d55f0f
Derek <Derek@192.168.200.128>
2015-09-10 17:35:41 -0700

    Added more comments to Student class; improved formatting for toString() method

--- Student.php
+++ Student.php
@@ -7,11 +7,13 @@
  */
 
 /**
- * Description of Student
+ * Holds first name, surname, emails and grades of student; can calculate
+ * grade average of student; can output description of student using toString()
  *
  * @author Derek
  */
 class Student {
+    // Default constructor
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -19,14 +21,17 @@ class Student {
         $this->grades = array();
     }
     
+    // Add email to Student's email array
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
 
+    // Add grade to Student's grades array
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    // Calculates the Student's average grade
     function average() {
         $total = 0;
         foreach ($this->grades as $value) {
@@ -35,11 +40,12 @@ class Student {
         return $total / count($this->grades);
     }
     
+    // Displays Student object (includes formatting)
     function toString() {
-        $result = $this->first_name . ' ' . $this->surname;
+        $result = '<strong>' . $this->first_name . ' ' . $this->surname . '</strong>';
         $result .= ' (' . $this->average() . ")\n";
         foreach($this->emails as $which=>$what) {
-            $result .= $which . ': ' . $what . "\n";
+            $result .= '<strong>' . $which . ":</strong>\t " . $what . "\n";
         }
         $result .= "\n";
         return '<pre>' . $result . '</pre>';
